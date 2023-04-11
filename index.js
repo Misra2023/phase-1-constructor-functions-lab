642908248bb145dc5eb20d987d59db0a899ae0dc
misra2023 <mishazabdi@gmail.com>
2023-04-11 06:21:01 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+// Scooter class
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  
+  // Driver class
+  function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  
+  // PickupLocation class
+  function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+  
